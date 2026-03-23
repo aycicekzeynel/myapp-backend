32e3ad3febac0378d04203274c21d99d99bf894a
aycicekzeynel <aycicekzeynel@gmail.com>
2026-03-23 21:17:49 +0300

    initial

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ io.on('connection', (socket) => {
 // ================================================
 
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '0.0.0.0';
 
 const startServer = async () => {
   try {
